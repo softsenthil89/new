a84b2b290a6fdc7a5800afa38d9b3f0b3a309287
root <root@ip-172-31-15-94.ap-south-1.compute.internal>
2022-06-11 04:55:58 +0000

    First commit for Hello.java file

---
+++ Hello.java
@@ -0,0 +1,12 @@
+
+public class Hello {
+
+	public static void main(String[] args) {
+		for(int i=1;i<=5;i++)
+		{
+		// TODO Auto-generated method stub
+System.out.println("Hello Mr.Senthilkumar, Java welcomes you");
+	}
+	}
+}
+
