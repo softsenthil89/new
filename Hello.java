e2042ba6865c87d5d357cb2207f6559b91fe64b7
root <root@ip-172-31-15-94.ap-south-1.compute.internal>
2022-06-11 05:14:32 +0000

    Second commit

--- Hello.java
+++ Hello.java
@@ -2,7 +2,7 @@
 public class Hello {
 
 	public static void main(String[] args) {
-		for(int i=1;i<=5;i++)
+		for(int i=0;i<=5;i++)
 		{
 		// TODO Auto-generated method stub
 System.out.println("Hello Mr.Senthilkumar, Java welcomes you");
